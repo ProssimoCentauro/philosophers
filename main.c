b196d84bdbf1d206a02bad3d189e954381fda4d4
ProssimoCentauro <rik.todaro@gmail.conm>
2025-03-18 13:04:40 +0100

    norminetted managers and main

--- main.c
+++ main.c
@@ -12,25 +12,9 @@ int	main(int ac, char **av)
 {
 	t_table	table;
 
-	// args checker
 	check_usage(ac, av);
-	// parse args
 	set_the_table(&table, av);
-	// data init
 	init_table(&table);
-	// dinner_start
-	// clean
-	//	printf("%lu", get_timestamp());
-	/*int i =-1;
-    while (++i < table.philo_nbr)
-    {
-        printf("philo %d:\n", table.philos[i].id);
-        printf("l_fork %d:\n", table.philos[i].left_fork->id);
-        printf("r_fork %d:\n\n", table.philos[i].right_fork->id);
-    }*/
-    //printf("\n");
-    //exit(0);
-    start_philos(&table);
-	//printf("%d\n", *table.philos[1].id);
+	start_philos(&table);
 	return (0);
 }
--- managers.c
+++ managers.c
@@ -1,37 +1,37 @@
 #include "philo.h"
 
-void    mutex_manager(pthread_mutex_t *mtx, t_codes code)
+void	mutex_manager(pthread_mutex_t *mtx, t_codes code)
 {
-    int ret;
+	int	ret;
 
-    ret = 1;
-    if (code == INIT)
-        ret = pthread_mutex_init(mtx, NULL);
-    else if (code == DESTROY)
-	ret = pthread_mutex_destroy(mtx);
-    else if (code == LOCK)
-        ret = pthread_mutex_lock(mtx);
-    else if (code == UNLOCK)
-        ret = pthread_mutex_unlock(mtx);
-    if (ret != 0)
-    	{
+	ret = 1;
+	if (code == INIT)
+		ret = pthread_mutex_init(mtx, NULL);
+	else if (code == DESTROY)
+		ret = pthread_mutex_destroy(mtx);
+	else if (code == LOCK)
+		ret = pthread_mutex_lock(mtx);
+	else if (code == UNLOCK)
+		ret = pthread_mutex_unlock(mtx);
+	if (ret != 0)
+	{
 		printf("error: %d\n", ret);
-        	exit_error("MUTEX ERROR!");
+		exit_error("MUTEX ERROR!");
 	}
 }
 
-void    thread_manager(pthread_t *thread, void *(*f)(void *),
-        void *arg, t_codes code)
+void	thread_manager(pthread_t *thread, void *(*f)(void *), void *arg,
+		t_codes code)
 {
-    int ret;
+	int	ret;
 
-    ret = 1;
-    if (code == CREATE)
-        ret = pthread_create(thread, NULL, f, arg);
-    else if (code == DETACH)
-        ret = pthread_detach(*thread);
-    else if (code == JOIN)
-        ret = pthread_join(*thread, NULL);
-    if (ret != 0)
-        exit_error("THREAD ERROR!");
+	ret = 1;
+	if (code == CREATE)
+		ret = pthread_create(thread, NULL, f, arg);
+	else if (code == DETACH)
+		ret = pthread_detach(*thread);
+	else if (code == JOIN)
+		ret = pthread_join(*thread, NULL);
+	if (ret != 0)
+		exit_error("THREAD ERROR!");
 }
